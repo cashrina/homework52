2392848d7fdf12fbd49f4d5e0b952fa86003bed0
cashrina <khripunova.irina.24@gmail.com>
2024-05-18 16:50:37 +0600

    Приступила к выполнению 1 уровня

---
+++ my-react-app/src/components/Card/Card.tsx
@@ -0,0 +1,10 @@
+const Card = () => {
+    return (
+        <span className="card rank-k diams">
+            <span className="rank">K</span>
+            <span className="suit">♦</span>
+        </span>
+    );
+};
+
+export default Card;
