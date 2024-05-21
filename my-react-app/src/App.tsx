a27e999c105404daa792847bed5d6a21bb30f57e
cashrina <khripunova.irina.24@gmail.com>
2024-05-22 00:58:13 +0600

    Закончила 4 уровень

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -2,15 +2,21 @@ import './App.css';
 import {useState} from "react";
 import CardDeck from "../src/lib/CardDeck.ts";
 import Card from "./lib/Card.ts";
+import PokerHand from "./lib/PokerHand.ts";
 import CardView from "./components/Card/CardView.tsx";
 
 const App = () => {
     const [allCards, setAllCards] = useState<Card[]>([]);
+    const [handOutcome, setHandOutcome] = useState<string>("");
 
     const dealCards = () => {
         const deck = new CardDeck();
         const cards = deck.getCards(5);
         setAllCards(cards);
+
+        const pokerHand = new PokerHand(cards);
+        const outcome = pokerHand.getOutcome();
+        setHandOutcome(outcome);
     };
 
     if (allCards.length === 0) {
@@ -20,12 +26,16 @@ const App = () => {
     }
 
     return (
+        <div>
+
               <div className="playingCards faceImages">
                   {allCards.map((card:Card, index:number) => (
                     <CardView key={index} rank={card.rank} suit={card.suit}/>
                   ))}
               </div>
-          );
+            <p>result: {handOutcome}</p>
+        </div>
+    );
 };
 
 export default App;
--- my-react-app/src/lib/PokerHand.ts
+++ my-react-app/src/lib/PokerHand.ts
@@ -1,9 +1,110 @@
 import Card from "../lib/Card.ts";
 
-class PokerHand {
+class PokerHand{
     private cards: Card[];
+    private readonly cardRank: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
 
     constructor(cards: Card[]) {
         this.cards = cards;
     }
+    isOnePair() {
+        const ranks = this.cards.map((card) => {
+            return card.rank;
+        });
+        for (const rank of ranks) {
+            const cardsWithRank = ranks.filter((cardRank) => {
+                return cardRank === rank;
+            });
+            if (cardsWithRank.length === 2) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isTwoPairs() {
+        const ranks = this.cards.map((card) => {
+            return card.rank;
+        });
+        let pairsCount = 0;
+        const checkedRanks: string[] = [];
+
+        for (const rank of ranks) {
+            let found = false;
+            for (const checkedRank of checkedRanks) {
+                if (checkedRank === rank) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found && ranks.filter((cardRank) => {
+                return cardRank === rank;
+            }).length === 2) {
+                pairsCount++;
+                checkedRanks.push(rank);
+            }
+
+        }
+        return pairsCount === 2;
+    }
+
+    isThreeOfAKind() {
+        const ranks = this.cards.map((card) => {
+            return card.rank;
+        });
+        for (const rank of ranks) {
+            if (ranks.filter(cardRank => {
+                return cardRank === rank;
+            }).length === 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    isFlush() {
+        const suit = this.cards[0].suit;
+        for (let i = 1; i < this.cards.length; i++) {
+            if (this.cards[i].suit !== suit) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    isStraightFlush() {
+        const suit = this.cards[0].suit;
+        for (let i = 1; i < this.cards.length; i++) {
+            if (this.cards[i].suit !== suit) {
+                return false;
+            }
+        }
+        const sortedRanks = this.cards.map(card => card.rank).sort((a, b) => {
+            return this.cardRank.indexOf(a) - this.cardRank.indexOf(b);
+        });
+        for (let i = 0; i < sortedRanks.length - 1; i++) {
+            if (this.cardRank.indexOf(sortedRanks[i + 1]) - this.cardRank.indexOf(sortedRanks[i]) !== 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+    getOutcome() {
+        switch (true) {
+            case this.isStraightFlush():
+                return "Straight flush";
+            case this.isFlush():
+                return "Flush";
+            case this.isThreeOfAKind():
+                return "Three of a kind";
+            case this.isTwoPairs():
+                return "Two pairs";
+            case this.isOnePair():
+                return "One pair";
+            default:
+                return "High card";
+        }
+    }
 }
+
+export default PokerHand;
