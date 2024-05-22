dd0660606f3cb66783596a69cdc4364453acd7d7
cashrina <khripunova.irina.24@gmail.com>
2024-05-22 17:30:35 +0600

    Прописала логику для кнопки "Раздать карты"

--- my-react-app/src/App.tsx
+++ my-react-app/src/App.tsx
@@ -8,8 +8,13 @@ import CardView from "./components/Card/CardView.tsx";
 const App = () => {
     const [allCards, setAllCards] = useState<Card[]>([]);
     const [handOutcome, setHandOutcome] = useState<string>("");
+    const [cardsLeftInDeck, setCardsLeftInDeck] = useState<number>(52);
 
     const dealCards = () => {
+        if (cardsLeftInDeck === 0) {
+            return;
+        }
+
         const deck = new CardDeck();
         const cards = deck.getCards(5);
         setAllCards(cards);
@@ -17,22 +22,26 @@ const App = () => {
         const pokerHand = new PokerHand(cards);
         const outcome = pokerHand.getOutcome();
         setHandOutcome(outcome);
-    };
 
-    if (allCards.length === 0) {
-        return (
-            <button onClick={dealCards}>Раздать карты</button>
-        );
-    }
+        setCardsLeftInDeck(cardsLeftInDeck - 5);
+    };
 
     return (
         <div>
-              <div className="playingCards faceImages">
-                  {allCards.map((card:Card, index:number) => (
-                    <CardView key={index} rank={card.rank} suit={card.suit}/>
-                  ))}
-              </div>
-            <p>Текущая рука: {handOutcome}</p>
+            {cardsLeftInDeck > 0 && (
+                <button onClick={dealCards}>Раздать карты</button>
+            )}
+            {allCards.length > 0 && (
+                <div>
+                    <div className="playingCards faceImages">
+                        {allCards.map((card: Card, index: number) => (
+                            <CardView key={index} rank={card.rank} suit={card.suit}/>
+                        ))}
+                    </div>
+                    <p>Текущая рука: {handOutcome}</p>
+                </div>
+            )}
+            {cardsLeftInDeck === 0 && <p>Карт в колоде больше нет</p>}
         </div>
     );
 };
