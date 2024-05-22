53d3fff89a416d9d887e63e314d9b906f345608e
cashrina <khripunova.irina.24@gmail.com>
2024-05-22 16:53:31 +0600

    Изменила логику в PokerHand.ts

--- my-react-app/src/lib/PokerHand.ts
+++ my-react-app/src/lib/PokerHand.ts
@@ -2,20 +2,15 @@ import Card from "../lib/Card.ts";
 
 class PokerHand{
     private cards: Card[];
-    private readonly cardRank: string[] = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'j', 'q', 'k', 'a'];
-
     constructor(cards: Card[]) {
         this.cards = cards;
     }
     isOnePair() {
-        const ranks = this.cards.map((card) => {
-            return card.rank;
-        });
+        const ranks = this.cards.map(card => card.rank);
+        const countMap: {[key: string]: number } = {};
         for (const rank of ranks) {
-            const cardsWithRank = ranks.filter((cardRank) => {
-                return cardRank === rank;
-            });
-            if (cardsWithRank.length === 2) {
+            countMap[rank] = (countMap[rank] || 0) + 1;
+            if (countMap[rank] === 2) {
                 return true;
             }
         }
@@ -26,24 +21,14 @@ class PokerHand{
         const ranks = this.cards.map((card) => {
             return card.rank;
         });
+        const countMap: {[key: string]: number } = {};
         let pairsCount = 0;
-        const checkedRanks: string[] = [];
-
         for (const rank of ranks) {
-            let found = false;
-            for (const checkedRank of checkedRanks) {
-                if (checkedRank === rank) {
-                    found = true;
-                    break;
-                }
-            }
-            if (!found && ranks.filter((cardRank) => {
-                return cardRank === rank;
-            }).length === 2) {
+            countMap[rank] = (countMap[rank] || 0) + 1;
+            if (countMap[rank] === 2) {
                 pairsCount++;
-                checkedRanks.push(rank);
+                countMap[rank] = 0;
             }
-
         }
         return pairsCount === 2;
     }
@@ -52,10 +37,10 @@ class PokerHand{
         const ranks = this.cards.map((card) => {
             return card.rank;
         });
+        const countMap: { [key: string]: number } = {};
         for (const rank of ranks) {
-            if (ranks.filter(cardRank => {
-                return cardRank === rank;
-            }).length === 3) {
+            countMap[rank] = (countMap[rank] || 0) + 1;
+            if (countMap[rank] === 3) {
                 return true;
             }
         }
@@ -72,27 +57,8 @@ class PokerHand{
         return true;
     }
 
-    isStraightFlush() {
-        const suit = this.cards[0].suit;
-        for (let i = 1; i < this.cards.length; i++) {
-            if (this.cards[i].suit !== suit) {
-                return false;
-            }
-        }
-        const sortedRanks = this.cards.map(card => card.rank).sort((a, b) => {
-            return this.cardRank.indexOf(a) - this.cardRank.indexOf(b);
-        });
-        for (let i = 0; i < sortedRanks.length - 1; i++) {
-            if (this.cardRank.indexOf(sortedRanks[i + 1]) - this.cardRank.indexOf(sortedRanks[i]) !== 1) {
-                return false;
-            }
-        }
-        return true;
-    }
     getOutcome() {
         switch (true) {
-            case this.isStraightFlush():
-                return "Стрит";
             case this.isFlush():
                 return "Флэш";
             case this.isThreeOfAKind():
